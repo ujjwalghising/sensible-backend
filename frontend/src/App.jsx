9df221b8e8a55dfebc849ed6e1562f1918dff8de
ujjwalghising <ujjwalghising567@gmail.com>
2025-03-06 18:50:38 +0530

    Fix Routes import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import AppRoutes from "./Routes";
+import AppRoutes from "./routes.jsx";
 
 const App = () => {
   return (
