cff1761811c20e624228abb11b89ebd4bcbdffab
unknown <ujjwalghising567@gmail.com>
2025-04-13 17:21:31 +0530

    heeloo

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(
     origin: [
       "http://localhost:5173", 
       "http://localhost:5174", 
+      "http://localhost:5175",
       "https://sensible-frontned.vercel.app"
     ],
     credentials: true,  // must be true to send cookies
